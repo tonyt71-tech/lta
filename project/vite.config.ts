a9b2575bec766431b41d99b390d1498525a61916
2catcodes <terrillcaden5@gmail.com>
2025-08-01 13:41:49 -0500

    idek

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: './', // ← make sure it's this!
   plugins: [react()],
+  base: "/", // VERY IMPORTANT for custom domain like www.lawtontennisacademy.com
 });
