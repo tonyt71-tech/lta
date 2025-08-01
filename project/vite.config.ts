9e9c501a85828c3e46ff6473ae9e2c5d346d15da
2catcodes <terrillcaden5@gmail.com>
2025-08-01 13:30:33 -0500

    Update vite.config.ts

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/', // ← make sure it's this!
+  base: './', // ← make sure it's this!
   plugins: [react()],
 });
