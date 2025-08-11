bb6685ebd8943ee7e7297c3f51d8a1a62553798d
2catcodes <terrillcaden5@gmail.com>
2025-08-11 08:58:07 -0500

    Update vite.config.ts

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// File: project/vite.config.ts
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
-  build: { outDir: "dist", emptyOutDir: true }
-});
+  base: '/', // Required for custom domain root
+})
